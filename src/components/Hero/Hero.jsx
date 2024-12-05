6965b9cf1b3fbf382d3d5df91aee4cdc9f1fc7a5
mhmtkarakas <m.karakas32@gmail.com>
2024-12-05 09:36:59 +0300

    Headers&Hero components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,12 +5,18 @@ import React from "react";
 import "./Hero.css";
 
 const Hero = () => {
+  const scrollToAboutUs = () => {
+    const aboutUsSection = document.getElementById("about-us");
+    if (aboutUsSection) {
+      aboutUsSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div className="hero-container">
       {/* Arka Plan Resmi */}
       <div className="hero-background">
         <img
-          src={heroimg} /* Burada resim yolunu belirtin */
+          src={heroimg} 
           alt="Ev Dekorasyonu"
           className="hero-image"
         />
@@ -20,6 +26,24 @@ const Hero = () => {
       <div className="hero-text">
        <p>Mardin Türkiye</p>
         <h1>Ev Dekorasyonu <br/> Bu Kadar <br/> Kolay Olmamıştı</h1>
+      </div>
+      <div className="hero-bottom">
+        <div className="hero-bottom-left">
+        <div className="mouse-icon" onClick={scrollToAboutUs}>
+            <span className="mouse-wheel"></span>
+          </div>
+        </div>
+        <div className="hero-bottom-right">
+               {/* Sol Metin */}
+        <div className="text-left animated-text">
+          Homeownership never looked so affordable
+        </div>
+
+        {/* Sağ Metin */}
+        <div className="text-right animated-text">
+          World-class architecture design studio
+        </div>
+        </div>
       </div>
     </div>
   );
